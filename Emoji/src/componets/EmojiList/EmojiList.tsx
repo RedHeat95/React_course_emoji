3bf5a3e8ab1963ef1278d27796a6d60bbd9ebc7f
Shundrik <Aleksandrx100@mail.ru>
2022-02-03 11:01:52 +0300

    new-work-emoji

--- Emoji/src/componets/EmojiList/EmojiList.tsx
+++ Emoji/src/componets/EmojiList/EmojiList.tsx
@@ -7,39 +7,22 @@ import { Form } from "../Form/Form";
 export const EmojiContainer = () => {
   const [search, setSearch] = useState("");
 
-  const newEmojies = emojies.filter((emojies) => {
-    if (emojies.title.toLowerCase().includes(search.toLowerCase())) {
-      return (
-        <EmojiRow
-          title={emojies.title}
-          symbol={emojies.symbol}
-        />
-      );    
-    } else if (emojies.keywords.toLowerCase().includes(search.toLowerCase())) {
-      return (
-        <EmojiRow
-          title={emojies.title}
-          symbol={emojies.symbol}
-        />
-      );
+  const newEmojies = emojies.filter((emoji) => {
+    if (
+      emoji.title.toLowerCase().includes(search.toLowerCase()) ||
+      emoji.keywords.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return emoji;
     }
-  })
+  });
 
   return (
     <div className={styles.emojiList}>
       <h1>Emoji</h1>
-      <Form
-        search={search} 
-        setSearch={setSearch}
-      />
-      {newEmojies.map((emoji) => {        
-          return (
-            <EmojiRow
-              title={emoji.title}
-              symbol={emoji.symbol}
-            />
-          )
+      <Form search={search} setSearch={setSearch} />
+      {newEmojies.map((emoji) => {
+        return <EmojiRow title={emoji.title} symbol={emoji.symbol} />;
       })}
     </div>
-  ) 
-}
+  );
+};
